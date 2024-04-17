a8f656a4ee92325e5b291e8ba84739fc1eb63860
Rahul <rajamohanty12345@gmail.com>
2024-04-17 19:48:19 +0530

    used rate limiter in backend

--- server.ts
+++ server.ts
@@ -1,14 +1,27 @@
 import express from "express";
 import cors from "cors";
-import authRouter from "./routes/userRouter";
+import rateLimit from "express-rate-limit";
+
 import todoRouter from "./routes/todoRouter";
+import authRouter from "./routes/userRouter";
 
 const app = express();
 
 //body parsing middleware
 app.use(express.json());
+
+//for cross origin requests
 app.use(cors());
+
+//for disabling the x-powered-by header
 app.disable("x-powered-by");
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
 
 // Routes
 app.use("/api/v1/auth", authRouter);
